f95b8539f0591b915b965646d5ea33429d0c3bb9
marie5060 <marie5060@hotmail.com>
2022-09-27 14:15:47 +0200

    subject added in final message

--- Quete1/thanks.php
+++ Quete1/thanks.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "Merci " . $_POST["user_last_name"]. " de nous avoir contacté à propos de " . $_POST["user_first_name"]."<br>". PHP_EOL.
+echo "Merci " . $_POST["user_last_name"]. " " . $_POST["user_first_name"]. " de nous avoir contacté à propos de " . $_POST["user_subject"]."<br>". PHP_EOL.
 "Un de nos conseiller vous contactera soit à l’adresse " . $_POST["user_email"]. " ou par téléphone au " . $_POST["user_number"]. " dans les plus brefs délais pour traiter votre demande : " . $_POST["user_message"]. PHP_EOL;
 
