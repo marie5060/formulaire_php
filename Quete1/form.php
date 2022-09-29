0b541bf61be038cc9fec0e04435837b00287fa42
marie5060 <marie5060@hotmail.com>
2022-09-29 09:30:51 +0200

    securisation form before refacto

--- Quete1/form.php
+++ Quete1/form.php
@@ -1,34 +1,102 @@
-<form  action="thanks.php"  method="post">
-    <div>
-      <label  for="nom">Nom :</label>
-      <input  type="text"  id="lastName"  name="user_last_name">
-      <label  for="prenom">Prenom :</label>
-      <input  type="text"  id="prenom"  name="user_first_name">
-    </div>
-    <div>
-      <label  for="courriel">Courriel :</label>
-        <input  type="email"  id="courriel"  name="user_email">
-    </div>
-    <div>
-    <label  for="number">Numero :</label>
-        <input  type="tel"  id="number"  name="user_number"> 
-    </div>
-    <div> 
-        <label>Sujet</label>
-        <select name="user_subject"> 
-        <option valeur="php" >PHP</option>
-   <option valeur="js">Javascript</option>
-   <option valeur="ang">Angular</option>
-   <option valeur="other">Autre</option>
-    </select>
-    </div>
-    <div>
-      <label  for="message">Message :</label>
-      <textarea  id="message"  name="user_message"></textarea>
-    </div>
-    <div  class="button">
-      <button  type="submit">Envoyer votre message</button>
-    </div>
-  </form>
+<?php
+// define variables and set to empty values
+$lastNameErr = $firstNameErr = $emailErr = $subjectErr= $messageErr = $numberErr = "";
+$lastName = $firstName = $email =$subject = $message = $number = "";
 
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 
+var_dump($_POST);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["user_last_name"])) {
+    $lastNameErr = "Last name is required";
+  } else {
+    $lastName = test_input($_POST["user_last_name"]);
+  }}
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["user_first_name"])) {
+      $firstNameErr = "First name is required";
+    } else {
+      $firstName = test_input($_POST["user_last_name"]);
+    }}
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if (empty($_POST["email"])) {
+      $emailErr = "Email is required";
+    } else {
+      $email = test_input($_POST["email"]);
+      // check if e-mail address is well-formed
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Invalid email format";
+      }
+    }}
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  if (empty($_POST["user_number"])) {
+    $numberErr = "Number is required";
+  } else if(preg_match('/^[0-9]{10}+$/', $_POST["user_number"])) {
+    $numberErr = "Valid Phone Number";
+    } else {
+      $numberErr = "Invalid Phone Number";
+    }}
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  if (empty($_POST["user_message"])) {
+    $messageErr = "Message is required";
+  } else {
+    $comment = test_input($_POST["user_message"]);
+  }}
+?>
+
+<form method="post" action="thanks.php">
+    <div>
+        Nom :
+        <input type="text" name="user_last_name">
+        <span class="error">* <?php echo $lastNameErr;?></span>
+        <br><br>
+    </div>
+    <div>
+        Prenom :
+        <input type="text" id="prenom" name="user_first_name">
+        <span class="error">* <?php echo $firstNameErr;?></span>
+        <br><br>
+    </div>
+    <div>
+        Courriel :
+        <input type="email" id="courriel" name="user_email">
+        <span class="error">* <?php echo $emailErr;?></span>
+        <br><br>
+    </div>
+    <div>
+        Numero :
+        <input type="tel" id="number" name="user_number">
+        <span class="error">* <?php echo $numberErr;?></span>
+        <br><br>
+    </div>
+    <div>
+        <label>Language :</label>
+        <select name="user_subject">
+            <option valeur="php">PHP</option>
+            <option valeur="js">Javascript</option>
+            <option valeur="ang">Angular</option>
+            <option valeur="other">Autre</option>
+        </select>
+        <span class="error">* <?php echo $subjectErr;?></span>
+        <br><br>
+    </div>
+    <div>
+        Message :
+        <textarea id="message" name="user_message"></textarea>
+        <span class="error">* <?php echo $messageErr;?></span>
+        <br><br>
+    </div>
+    <div class="button">
+        <button type="submit">Envoyer votre message</button>
+    </div>
+</form>
